d7aa6971f7993eb11d7fa18134136832ba755a6b
Lucas Legrand <legrand.lucas0@gmail.com>
2021-02-10 18:51:04 +0100

    first data extract

--- src/Data.js
+++ src/Data.js
@@ -1,14 +1,38 @@
 import React from "react";
-import data from './data/data'
+import invyoData from './data/data'
+
+
+
 
 const Data = () => {
-  console.log(data)
+
+  // console.log(invyoData.articles.slice(0,5).map((article) => article.Title))
+  console.log(invyoData.articles.slice(0, 5).map((article) => article.Title))
 
   return (
     <div>
-      <h1>Data</h1>
+      <h1>Datas</h1>
+      {invyoData.articles.slice(0, 5).map((article, index) =>
+        <div>
+          <p key={index}>{article.Title}</p>
+          <p key={index}>{article.Content.substring(0, 50)}</p>
+          <p key={index}>{article.Langues}</p>
+          {article.Tags.topic.map((topic) => (
+            <p key={index}>{topic}</p>
+          ))}
+        </div>
+
+      )}
+      {/* {invyoData.articles.slice(0, 5).map((article) => (
+        <div>
+          <p>{article.Title}</p>
+          <p>{article.Content}</p>
+          <p>{article.Langues}</p>
+          <p>{article.Tags}</p>
+        </div>
+       ))} */}
     </div>
-  )
+  );
 }
 
 export default Data;
