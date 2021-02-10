f03fe2fa8a2bebdcd0c398ae9f83c2d52e6e92a0
Lucas Legrand <legrand.lucas0@gmail.com>
2021-02-10 20:11:58 +0100

    wip grid

--- src/Data.js
+++ src/Data.js
@@ -1,36 +1,79 @@
 import React from "react";
 import invyoData from './data/data'
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 
 
 
 const Data = () => {
 
-  // console.log(invyoData.articles.slice(0,5).map((article) => article.Title))
   console.log(invyoData.articles.slice(0, 5).map((article) => article.Title))
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+  }));
+
+  const classes = useStyles();
 
   return (
     <div>
-      <h1>Datas</h1>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+          <Typography variant="body1">Titre</Typography>
+        </Grid>
+        <Grid item xs={6}>
+        <Typography variant="body1">Contenu</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography variant="body1">Langues</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography variant="body1">Tags</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography variant="body1">Actions</Typography>
+    </Grid>
+    </Grid>
       {invyoData.articles.slice(0, 5).map((article, index) =>
-        <div>
-          <p key={index}>{article.Title}</p>
-          <p key={index}>{article.Content.substring(0, 50)}</p>
-          <p key={index}>{article.Langues}</p>
-          {article.Tags.topic.map((topic) => (
-            <p key={index}>{topic}</p>
-          ))}
-        </div>
-
-      )}
-      {/* {invyoData.articles.slice(0, 5).map((article) => (
-        <div>
-          <p>{article.Title}</p>
-          <p>{article.Content}</p>
-          <p>{article.Langues}</p>
-          <p>{article.Tags}</p>
-        </div>
-       ))} */}
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={3}>
+              <Typography variant="body2">
+              {article.Title}
+              </Typography>
+              <Typography variant="body2">
+              {"Source:" + article.url.substring(0, 10)}
+              </Typography>
+              <Typography variant="body2">
+              {article.Date}
+              </Typography>
+        </Grid>
+        <Grid item xs={6}>
+             {article.Content.substring(0, 220) + '...'}
+        </Grid>
+        <Grid item xs={1}>
+          {article.Language}
+        </Grid>
+        <Grid item xs={1}>
+              {article.Tags.topic.map((topic) => (
+                <p>{topic}</p>
+              ))}
+        </Grid>
+        <Grid item xs={1}>
+         actions
+        </Grid>
+      </Grid>
+    </div>
+    )}
     </div>
   );
 }
